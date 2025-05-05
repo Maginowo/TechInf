322852bcbd2f2e578a00723c30f08b1d23326f3f
Krzysztof Grzelak <maginowo@gmail.com>
2025-05-05 11:18:03 +0200

    Calculator added add operation and text menu

--- 05.05.2025 Calculator with parameters/main.cpp
+++ 05.05.2025 Calculator with parameters/main.cpp
@@ -1,16 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
-void usage(const char *program_name) {
-    printf("Sposob uzycia: %s <komunikat> <#powtorzen>\n", program_name);
-    exit(1);
+#include <string.h>
+
+void formatText(){
+    printf("Command format: <number1> <operation> <number2>\n");
+    printf("Please provide parameters.\n");
+    printf("Possible operations: add\n");
 }
+
+int add(int number1, int number2){
+    return number1 + number2;
+}
+
 int main(int argc, char const *argv[]) {
-    int i, count;
-    if (argc < 3)
-        usage(argv[0]);
-    count = atoi(argv[2]);
-    printf("Powtarzam %d razy..\n", count);
-    for (i = 0; i < count; i++)
-        printf("%3d - %s\n", i, argv[1]);
+    int number1, number2;
+    if (argc < 4){
+        formatText();
+        return 0;
+    }
+    number1 = atoi(argv[1]);
+    number2 = atoi(argv[3]);
+
+    printf("Operation: %d %s %d\n", number1, argv[2], number2);
+
+    if(strcmp(argv[2], "add") == 0){
+        printf("Result: %d", add(number1, number2));
+    } else {
+        formatText();
+    }
+
+
     return 0;
 }

075b0cdc3f9f2e2d7f48bf827f9e234118eb7c7c
Krzysztof Grzelak <maginowo@gmail.com>
2025-05-05 10:45:14 +0200

    Calculator with parameters isn't yet a calculator, but now it repeats a messege x times

--- 05.05.2025 Calculator with parameters/main.cpp
+++ 05.05.2025 Calculator with parameters/main.cpp
@@ -1,9 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+void usage(const char *program_name) {
+    printf("Sposob uzycia: %s <komunikat> <#powtorzen>\n", program_name);
+    exit(1);
+}
 int main(int argc, char const *argv[]) {
-    int i;
-    printf("Podano %d argumentow:\n", argc);
-    for (i = 0; i < argc; i++) {
-        printf("Argunent #%d\t-\t%s\n", i, argv[i]);
-    }
+    int i, count;
+    if (argc < 3)
+        usage(argv[0]);
+    count = atoi(argv[2]);
+    printf("Powtarzam %d razy..\n", count);
+    for (i = 0; i < count; i++)
+        printf("%3d - %s\n", i, argv[1]);
     return 0;
 }

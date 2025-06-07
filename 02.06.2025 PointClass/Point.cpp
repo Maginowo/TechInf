5007dfe02718adde90173587fbbe2336327019f3
Komputer domowy <maginowo@gmail.com>
2025-06-07 13:29:03 +0200

    Version 3 of point class complete

--- 02.06.2025 PointClass/Point.cpp
+++ 02.06.2025 PointClass/Point.cpp
@@ -5,8 +5,8 @@
 Point::Point() {
     //std::cout << "Wywolano konstruktor domyslny\n";
     m_name = "bez nazwy";
-    m_x = 0;
-    m_y = 0;
+    m_x = 1;
+    m_y = 1;
 }
 Point::Point(const std::string& name, double x, double y) {
     //std::cout << "Wywolano konstruktor z argumentem " << name << std::endl;
@@ -39,7 +39,9 @@ double Point::distance(const Point& p) const {
 const Point& Point::distant(const Point& p) const {
     double d = sqrt( m_x * m_x + m_y * m_y);
     double dp = sqrt(p.m_x * p.m_x + p.m_y * p.m_y);
+
     if (d > dp)
         return *this;
+
     return p;
 }
--- 02.06.2025 PointClass/main.cpp
+++ 02.06.2025 PointClass/main.cpp
@@ -1,24 +1,16 @@
 #include <iostream>
 #include "Point.h"
-
 using namespace std;
-int main() {
-    Point p1;
-    Point p2("P2",30,40);
-    Point p3 = Point("P3");
-    Point p4 = {"P4", 30, 40};
-    Point p5 {};
 
-    p1.setName("P1");
-    p1.setXY(10, 20);
-    p2.setXY(30, 40);
+int main() {
+    Point p1 = Point("P1", 0, 0);
+    Point p2 = Point("P2", 30, 40);
 
     p1.show();
     p2.show();
-    p3.show();
-    p4.show();
-    p5.show();
 
-    p2 = p1;
-    p2.show();
+    cout << "Odleglosc pomiedzy punktami wynosi " << p1.distance(p2) << endl;
+    const Point& p = p1.distant(p2);
+    cout << "Punkt bardziej oddalony od poczatku ukladu wspolrzednych to: " << endl;
+    p.show();
 }

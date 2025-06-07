68190b2298eabf1c0b4256201f85866e4fb5802a
Komputer domowy <maginowo@gmail.com>
2025-06-07 14:02:00 +0200

    Translated console printouts to english and made the points have randomized values. Also created a simple loop which returns the furthest point from the (0, 0) coordinate.

--- 02.06.2025 PointClass/main.cpp
+++ 02.06.2025 PointClass/main.cpp
@@ -1,24 +1,43 @@
 #include <iostream>
 #include "Point.h"
+#include <random>
 using namespace std;
 
 int main()
 {
-    Point points[10] = {
-        Point("P1", 0, 0),
-        Point("P2", 10, 20),
-        Point("P3", 30, 40)
-        };
-    cout << "Istnieje " << Point::numberOfPoints() << " punktow."<< endl;
-    for (int i = 0; i < 10; i++)
-    {
+    // defining random generator
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(0, 100);
+
+
+    Point points[10] = {};
+
+    for (int i = 0; i < 10; i++) {
+        double randomNumber = dis(gen);
+        points[i] = Point("P" + to_string(i+1), randomNumber, randomNumber);
+    }
+
+    cout << "There are " << Point::numberOfPoints() << " points."<< endl;
+
+    // Showing all the points
+    for (int i = 0; i < 10; i++){
         points[i].show();
     }
 
+    // Looking for the furthest point from the (0, 0)
+    const Point* furthestPoint = &points[0];
+    for (int i = 0; i < 10; i++) {
+        furthestPoint = &furthestPoint->distant(points[i]);
+    }
+    cout << endl << "Furthest point from (0, 0) is: " << endl;
+    furthestPoint->show();
+
+    // Creating new points in the block, which means that the object lifetime will be restricted to only inside the block
     {
         Point p1, p2, p3;
         cout << endl;
-        cout << "Teraz istnieje " << Point::numberOfPoints() << " punktow." << endl;
+        cout << "Now there are " << Point::numberOfPoints() << " points." << endl;
     }
-    cout << "A teraz istnieje " << Point::numberOfPoints() << " punktow." << endl;
+    cout << "Now there are " << Point::numberOfPoints() << " points." << endl;
 }
